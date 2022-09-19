11f3510fc4f48757d5e93e2cab8f4f7093ee3d4d
edivan <edivansantos1478@gmail.com>
2022-09-19 15:47:48 -0300

    criando um banco

--- prisma/migrations/script.ts
+++ prisma/migrations/script.ts
@@ -3,15 +3,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+    // ... você escreverá suas consultas do Prisma Client aqui
+    const user = await prisma.user.create({
+        data: {
+          name: 'Alice',
+          email: 'alice@prisma.io',
+        },
+    })
+    console.log(user)
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
