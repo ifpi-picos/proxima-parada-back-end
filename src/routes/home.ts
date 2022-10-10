55100bfc987eb33e0e998519ab0e2e441fdcfa57
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-10-09 21:29:06 -0300

    Adição e configuração do editorconfig, eslint e prettier

--- src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const routers = Router();
-
-routers.get("/", (req: Request, res: Response) => {
-  res.json({ caronas: "Lista de todas as caronas!!" });
-});
-
-export default routers;
