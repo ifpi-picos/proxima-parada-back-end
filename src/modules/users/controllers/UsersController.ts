d4b6d751c63c845f24ebec8b7a11293f02930c3e
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-10-18 21:35:13 -0300

    Criação do serviço de atualizar as informações do usuário

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import CreateUserService from "../services/CreateUserService";
+import UpdateUserService from "../services/UpdateUserService";
 
 export default class UsersController {
     public async create(
@@ -17,6 +18,30 @@ export default class UsersController {
                 email,
                 password,
                 samePasswords,
+                occupation,
+            })
+            .catch(error => {
+                response.statusCode = 400;
+                return error;
+            });
+
+        return response.json(user);
+    }
+
+    public async update(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { name, phone_number, occupation } = request.body;
+        const { id } = request.params;
+
+        const updateUserService = new UpdateUserService();
+
+        const user = await updateUserService
+            .execute({
+                id,
+                name,
+                phone_number,
                 occupation,
             })
             .catch(error => {
---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,60 @@
+import { prismaClient } from "../../../database/prismaClient";
+import AppError from "../../../shared/errors/AppError";
+
+interface IUserToUpdate {
+    id: string;
+    name: string;
+    phone_number: string;
+    occupation: string;
+}
+
+interface IUserUpdateToReturn {
+    id: string;
+    name: string;
+    phone_number: string;
+    occupation: string;
+}
+
+class UpdateUserService {
+    public async execute({
+        id,
+        name,
+        phone_number,
+        occupation,
+    }: IUserToUpdate): Promise<IUserUpdateToReturn> {
+        const userExists = await prismaClient.user.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!userExists) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const userUpdated = await prismaClient.user.update({
+            select: {
+                id: true,
+                name: true,
+                phone_number: true,
+                occupation: true,
+                avatar: true,
+                status: true,
+                created_at: true,
+                updated_at: true,
+            },
+            where: {
+                id: id,
+            },
+            data: {
+                name: name,
+                phone_number: phone_number,
+                occupation: occupation,
+            },
+        });
+
+        return userUpdated as IUserUpdateToReturn;
+    }
+}
+
+export default UpdateUserService;
