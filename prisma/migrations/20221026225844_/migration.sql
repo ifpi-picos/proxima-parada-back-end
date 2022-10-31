828fcb9bb5d018114c6139d21f6b66e3da66c546
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-10-31 12:30:56 -0300

    atualizando metodos de retorno de usuario pelo id e adicionando configurações de conexão com o client

--- prisma/migrations/20221026225844_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "admin_users" ADD CONSTRAINT "admin_users_pkey" PRIMARY KEY ("id");
-
--- AlterTable
-ALTER TABLE "user_token" ADD CONSTRAINT "user_token_pkey" PRIMARY KEY ("user_tokenId");
