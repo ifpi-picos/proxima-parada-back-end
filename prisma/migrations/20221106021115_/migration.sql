f0f4059a22aed25fbe4abd4b0e311df81c0d8cad
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-05 23:34:21 -0300

    Remoção do atributo de descrição da tabela StatusRequest

---
+++ prisma/migrations/20221106021115_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `status_request` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "status_request" DROP COLUMN "description";
