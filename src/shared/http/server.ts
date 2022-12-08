cd36bc487c70e4a8edb5f5c3f07dd21169ed855d
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-12-08 09:32:58 -0300

    Alterando arquivos de server pra testes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,42 +1,7 @@
-import express, { NextFunction, Request, Response } from "express";
-import routes from "./routes";
-import cors from "cors";
-import AppError from "../errors/AppError";
-import cookieParser from "cookie-parser";
+import app from "./app";
 
 const port = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(express.json());
-const corsOptions = {
-    origin: process.env.BASE_URL_CLIENT,
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
-app.use("/api", routes);
-
-app.use(cookieParser());
-
-app.get("/api", (request, response) => {
-    response
-        .status(200)
-        .send('<h1 style="texte-align: center">Api Próxima Parada Online</h1>');
-});
-
-app.use(
-    (error: any, request: Request, response: Response, next: NextFunction) => {
-        console.log(error);
-        return response.status((error as AppError).statusCode ?? 500).send({
-            message: (error as AppError).message,
-            statusCode: (error as AppError).statusCode ?? 500,
-        });
-    },
-);
-
 app.listen(port, () => {
-    console.log("Servidor rodando na porta " + port);
+    console.log("Servidor rodando na porta: " + port);
 });
